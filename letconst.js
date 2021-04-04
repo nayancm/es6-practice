e24f36c6f1e8f0345635d4e93734e44a0ea07b94
Nayan CM <nayan15-10316@diu.edu.bd>
2021-04-04 13:00:12 +0600

    practice wit let const

---
+++ letconst.js
@@ -0,0 +1,15 @@
+const hubby = "Nayan";
+//hubby = "Malakar";
+console.log(hubby);
+
+let names = "Patuakhali";
+//names = "Daffodil";
+console.log(names);
+
+const values = [44, 22, 11];
+values[3] = "Nayan";
+values[5] = 55;
+values.push("Daffodil", 88);
+values.pop();
+console.log(values);
+
